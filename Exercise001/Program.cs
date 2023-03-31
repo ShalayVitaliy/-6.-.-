6a1374e991052b206a305f5b5b7be7854c4c13d2
Vitaliy <vitalik.shalay2002@yandex.ru>
2023-03-31 11:39:02 +0300

    Done Exercise001

---
+++ Exercise001/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+Console.Write("Enter the numbers separated by a space: ");
+int[] Array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+
+int Counter(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+            count++;
+    }
+    return count;
+}
+
+Console.Write($"The count of positive numbers: {Counter(Array)}");
+
