915b7ef4ed2bfa6f772e64df56052e8014d1477f
Vitaliy <vitalik.shalay2002@yandex.ru>
2023-03-31 12:31:28 +0300

    Done Exercise002

---
+++ Exercise002/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+
+const int COEFFICIENT = 0;
+const int CONSTANT = 1;
+const int xCoord = 0;
+const int yCoord = 1;
+const int Line1 = 1;
+const int Line2 = 2;
+
+double[] lineData1 = InputLineData(Line1);
+double[] lineData2 = InputLineData(Line2);
+
+double Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    double result = Convert.ToDouble(value);
+    return result;
+}
+
+double[] InputLineData(int numberOfLine)
+{
+    double[] lineData = new double[2];
+    lineData[COEFFICIENT] = Prompt($"Введите коэффициент для {numberOfLine} прямой : ");
+    lineData[CONSTANT] = Prompt($"Введите константу для {numberOfLine} прямой : ");
+    return lineData;
+}
+
+double[] FindCoords(double[] lineData1, double[] lineData2)
+{
+    double[] coord = new double[2];
+    coord[xCoord] = (lineData1[CONSTANT] - lineData2[CONSTANT]) / (lineData2[COEFFICIENT] - lineData1[COEFFICIENT]);
+    coord[yCoord] = lineData1[CONSTANT] * coord[xCoord] + lineData1[CONSTANT];
+    return coord;
+}
+
+bool ValidateLines (double[] lineData1, double[] lineData2)
+{
+    if (lineData1[COEFFICIENT] == lineData2[COEFFICIENT])
+    {
+        if (lineData1[CONSTANT] == lineData2[CONSTANT])
+        {
+            Console.WriteLine("Прямые совпадают.");
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("Прямые параллельны.");
+            return false;
+        }
+    }
+    return true;
+}
+
+if(ValidateLines(lineData1, lineData2))
+{
+    double[] coord = FindCoords(lineData1, lineData2);
+    Console.Write($"Точка пересечения уравнений y = {lineData1[COEFFICIENT]} * x + {lineData1[CONSTANT]} и y = {lineData2[COEFFICIENT]} * x + {lineData2[CONSTANT]}");
+    Console.WriteLine($" имеет координаты ({Math.Round(coord[xCoord], 3)}, {Math.Round(coord[yCoord], 3)})");
+}
